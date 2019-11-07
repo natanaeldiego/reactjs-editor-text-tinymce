e2049cebaf15a7b24e35f505f440e532eacee5ae
Natanael Diego <natanael@uds.com.br>
2019-11-07 13:23:09 -0300

    Editor de texto bonito e personalizado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Editor } from '@tinymce/tinymce-react';
 
 function App() {
+
+  function handleEditorChange(e){
+    console.log('Content was updated:', e.target.getContent());
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Editor
+      apiKey='your-api-key'
+      initialValue="<p>Conteúdo inicial vai aqui, não esquecça de ir no site: <a href='https://www.tiny.cloud/auth/signup/'>TinyMCE</a> para pegar your-api-key!</p>"
+      init={{
+        height: 500,
+        menubar: true,
+        plugins: [
+          'advlist autolink lists link image charmap print preview anchor',
+          'searchreplace visualblocks code fullscreen',
+          'insertdatetime media table paste code help wordcount'
+        ],
+        toolbar:
+          'undo redo | formatselect | bold italic backcolor | \
+          alignleft aligncenter alignright alignjustify | \
+          bullist numlist outdent indent | removeformat | help'
+      }}
+      onChange={handleEditorChange}
+    />
   );
 }
 
